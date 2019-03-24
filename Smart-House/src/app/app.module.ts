2c4912c90ca9ab58df027b19c5151875f50c9180
Guilherme Caldeira Godoy da Silva <guilherme.silva@saopaulo.netpartners>
2019-03-24 16:19:17 -0300

    esboço

--- Smart-House/src/app/app.module.ts
+++ Smart-House/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComodoComponent } from './Comodo/Comodo.component';
 import { NavbarComponent } from './Navbar/Navbar.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ComodoComponent,
-      NavbarComponent
+      NavbarComponent,
+      FooterComponent,
+      SearchComponent
    ],
    imports: [
       BrowserModule,
